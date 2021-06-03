c13df71c46e0d470636484a56b376ef5f95126d5
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-06-02 21:55:54 -0300

    Adicionando while externo

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ valores2 = [2, -2, 2, 1]
 objetivos = [0, 1, 0, 1]
 pesos = [0, 0, 0]
 
-for i in range(len(valores1)):
-    print(f'\nIteração: {i}')
+flag = True
+cont = 0
+while flag:
+    flag = False
+    print(f'\nIteração externa: {cont}')
+    for i in range(len(valores1)):
+        print(f'\nIteração: {i}')
 
-    soma = somaponderada(valores1[i], valores2[i], pesos[0], pesos[1], pesos[2])
-    print(f'Soma ponderada: {soma}')
+        soma = somaponderada(valores1[i], valores2[i], pesos[0], pesos[1], pesos[2])
+        print(f'Soma ponderada: {soma}')
 
-    parametrizacao = limiar(soma)
-    print(f'Parametrização: {parametrizacao}')
+        parametrizacao = limiar(soma)
+        print(f'Parametrização: {parametrizacao}')
 
-    erro = calculaerro(objetivos[i], parametrizacao)
-    print(f'Erro: {erro}')
+        erro = calculaerro(objetivos[i], parametrizacao)
+        print(f'Erro: {erro}')
 
-    if erro != 0:
-        pesos = atualizapesos(valores1[i], valores2[i],
-                                pesos[0], pesos[1], pesos[2], erro)
-    print(f'Pesos: {pesos}')
+        if erro != 0:
+            flag = True
+            pesos = atualizapesos(valores1[i], valores2[i],
+                                    pesos[0], pesos[1], pesos[2], erro)
+            
+        print(f'Pesos: {pesos}')
+    cont+=1
+
+print(f'\nAo final das iterações ocorreram:\n\tIterações:{cont}\n\tPesos: {pesos}')

a1f6b11ce5197d5fbffd46ebd1a4579556552cd9
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2023-12-01 17:12:40 -0300

    <FEATURE> Implements many neurons

--- main.py
+++ main.py
@@ -1,33 +1,73 @@
-from modules import *
+from modules import atualiza_pesos, calcula_erro, soma_ponderada, limiar
 
-valores1 = [2, -2, -2, -1]
-valores2 = [2, -2, 2, 1]
-objetivos = [0, 1, 0, 1]
-pesos = [0, 0, 0]
+entradas = [
+    {"valor": [1, 0, 0, 0], "nome": "Onça"},
+    {"valor": [1, 0, 0, 1], "nome": "Macaco"},
+    {"valor": [1, 0, 1, 0], "nome": "Jacaré"},
+    {"valor": [1, 0, 1, 1], "nome": "Lagarto"},
+    {"valor": [1, 1, 0, 0], "nome": "Avestruz"},
+    {"valor": [1, 1, 0, 1], "nome": "Pato"},
+    {"valor": [1, 1, 1, 0], "nome": "Sapo"},
+    {"valor": [1, 1, 1, 1], "nome": "Rã"},
+]
+
+classes = {
+    "00": "Mamífero",
+    "01": "Réptil",
+    "10": "Ave",
+    "11": "Anfíbio",
+}
+
+objetivos = [
+    [0,0],
+    [0,0],
+    [0,1],
+    [0,1],
+    [1,0],
+    [1,0],
+    [1,1],
+    [1,1],
+]
+
+num_entradas = len(entradas[0]['valor'])
+num_neuronios = len(objetivos[0])
+
+pesos_neuronios = [[0 for _ in range(num_entradas)] for _ in range(num_neuronios)]
+bias_neuronios = [0 for _ in range(num_neuronios)]
 
 flag = True
 cont = 0
-while flag:
+limite_iteracoes = 10
+
+while flag and cont < limite_iteracoes:
     flag = False
-    print(f'\nIteração externa: {cont}')
-    for i in range(len(valores1)):
-        print(f'\nIteração: {i}')
+    print(f'Iteração externa: {cont}')
 
-        soma = somaponderada(valores1[i], valores2[i], pesos[0], pesos[1], pesos[2])
-        print(f'Soma ponderada: {soma}')
+    for i in range(len(entradas)):
+        nome_animal = entradas[i]['nome']
+        valores = entradas[i]['valor']
 
-        parametrizacao = limiar(soma)
-        print(f'Parametrização: {parametrizacao}')
+        erros = []
+        somas = []
+        parametrizacoes = []
 
-        erro = calculaerro(objetivos[i], parametrizacao)
-        print(f'Erro: {erro}')
+        for n in range(num_neuronios):
+            soma = soma_ponderada(valores, pesos_neuronios[n], bias_neuronios[n])
+            somas.append(soma)
 
-        if erro != 0:
-            flag = True
-            pesos = atualizapesos(valores1[i], valores2[i],
-                                    pesos[0], pesos[1], pesos[2], erro)
-            
-        print(f'Pesos: {pesos}')
-    cont+=1
+            param = limiar(soma)
+            parametrizacoes.append(param)
 
-print(f'\nAo final das iterações ocorreram:\n\tIterações:{cont}\n\tPesos: {pesos}')
+            erro = calcula_erro(objetivos[i][n], param)
+            erros.append(erro)
+
+            if erro != 0:
+                flag = True
+                pesos_neuronios[n], bias_neuronios[n] = atualiza_pesos(valores, pesos_neuronios[n], bias_neuronios[n], erro)
+
+        print(f'\nPesos: {pesos_neuronios}, Bias: {bias_neuronios}, Erro: {erros}')
+        print(f'Animal: {nome_animal}, Resultado: {parametrizacoes == objetivos[i]}, Classe: {parametrizacoes}')
+        
+    cont += 1
+
+print(f'\nAo final das iterações ocorreram:\n\tIterações:{cont}\n\tPesos: {pesos_neuronios}\n\tBiases: {bias_neuronios}')
--- modules.py
+++ modules.py
@@ -1,26 +1,21 @@
-def calculaerro(v1, v2):
+def calcula_erro(v1, v2):
     return v1 - v2
 
+def soma_ponderada(valores, pesos, bias):
+    soma = 0
+    for i in range(len(valores)):
+        soma += valores[i] * pesos[i]
+    return soma + bias
 
-def somaponderada(valor1, valor2, peso1, peso2, bias):
-    return valor1 * peso1 + valor2 * peso2 + bias
-
-
-def limiar(v):
-    if v >= 0:
+def limiar(v, t=0):
+    if v > t:
         return 1
     return 0
 
-
-def atualizapesos(valor1, valor2, peso1, peso2, bias, erro):
-    peso1 = peso1 + erro * valor1
-    peso2 = peso2 + erro * valor2
-    bias = bias + erro
-    return [peso1, peso2, bias]
-
-
-if __name__ == '__main__':
-    print(calculaerro(2, 1) == 1)
-    print(somaponderada(2, 3, 1, 2, 4) == 12)
-    print(limiar(1) == 1)
-    print(atualizapesos(1, 3, 1, 2, 4, 2) == [3, 8, 6])
+def atualiza_pesos(valores, pesos, bias, erro):
+    novos_pesos = []
+    for i in range(len(valores)):
+        novos_pesos.append(pesos[i] + erro * valores[i])
+        
+    novo_bias = bias + erro
+    return novos_pesos, novo_bias
